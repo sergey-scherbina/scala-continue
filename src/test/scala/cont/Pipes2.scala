cefa0f540ab1928a083a469ddf0a1bd9833ec7c8
Sergiy Shcherbyna <sscherbina@luxoft.com>
2019-10-10 15:42:32 +0200

    tco (pipes)

---
+++ src/test/scala/cont/Pipes2.scala
@@ -0,0 +1,94 @@
+package cont
+
+import java.io.{BufferedReader, StringReader}
+
+import scala.util.Try
+import scala.util.control.TailCalls._
+
+object Pipes2 extends App {
+
+  trait Read[A] {
+    def read(s: String): A
+
+    def readLine(r: BufferedReader): A = read(r.readLine())
+  }
+
+  object Read {
+    def apply[A: Read]: Read[A] = implicitly[Read[A]]
+
+    implicit object readInt extends Read[Int] {
+      override def read(s: String): Int = s.toInt
+    }
+
+  }
+
+  type ![A] = TailRec[A]
+  type =>>[A, B] = A => ![B]
+
+  final case class Cont[A, S, R](cont: ![(A =>> S) =>> R]) extends AnyVal {
+    @inline def flatMap[B, S1](f: A => Cont[B, S1, S]): Cont[B, S1, R] =
+      Cont(done(k => cont.flatMap(_ (f(_).cont.flatMap(_ (k))))))
+
+    @inline def map[B](f: A => B): Cont[B, S, R] = Cont(done(k => cont.flatMap(_ (a => k(f(a))))))
+
+    @inline def >>=[B, C](f: A => Cont[B, C, S]): Cont[B, C, R] = flatMap(f)
+
+    @inline def >>[B, C](c2: Cont[B, C, S]): Cont[B, C, R] = flatMap(_ => c2)
+
+    @inline def >>>(s: S): Cont[S, S, R] = map(_ => s)
+  }
+
+  type :#:[A, R] = Cont[A, R, R]
+
+  @inline final def pure[A, R](a: A): A :#: R = Cont(done(_ (a)))
+
+  @inline final def shift0[A, S, R](k: (A =>> S) =>> R): Cont[A, S, R] = Cont(done(k))
+
+  @inline final def reset[A, R](k: Cont[A, A, R]): R = k.cont.result(done).result
+
+  @inline final def shift[A, S, R](f: (A => S) => R): Cont[A, S, R] = shift0(k => done(f(k(_).result)))
+
+  type ~>[A, B] = A => B => ![Unit]
+
+  trait InCont[I] extends (Unit ~> OutCont[I])
+
+  def InCont[I](k: (Unit ~> OutCont[I])): InCont[I] = k(_)
+
+  trait OutCont[O] extends (O ~> InCont[O])
+
+  def OutCont[O](k: O ~> InCont[O]): OutCont[O] = k(_)
+
+  type Pipe[I, O, A] = A :#: (InCont[I] ~> OutCont[O])
+
+  def input[I, O]: Pipe[I, O, I] = shift(k => ki => ko =>
+    ki()(OutCont(i => k1 => k(i)(k1)(ko))))
+
+  def output[I, O](o: O): Pipe[I, O, Unit] = shift(k => ki => ko =>
+    ko(o)(InCont(_ => k1 => k()(ki)(k1))))
+
+  def merge[I, O, M, A](p: Pipe[I, M, A], q: Pipe[M, O, A]): Pipe[I, O, A] =
+    shift(k => ki => ko => q.cont.result(_ => ???).result.apply(
+      InCont(_ => k1 => p.cont.result(_ => ???).result.apply(ki)(k1)))(ko))
+
+  def runPipeIO[I: Read, O](p: Pipe[I, O, Unit])(r: BufferedReader) = {
+    lazy val ki: InCont[I] = InCont(_ => _ (Read[I].readLine(r))(ki))
+    lazy val ko: OutCont[O] = OutCont { o => k => println(o); k()(ko) }
+    p.cont.result(_ => done(_ => _ => done(()))).result(ki)(ko).result
+  }
+
+  //////
+
+  def double(): Pipe[Int, Int, Unit] = for {
+    i <- input[Int, Int]
+    _ <- output[Int, Int](i * 2)
+    _ <- double()
+  } yield ()
+
+  def quad(): Pipe[Int, Int, Unit] = merge(double(), double())
+
+  println(Try(runPipeIO(double())(
+    new BufferedReader(new StringReader(
+      "1\n" * 1000
+    )))))
+
+}
--- src/test/scala/cont/PipesTest.scala
+++ src/test/scala/cont/PipesTest.scala
@@ -20,68 +20,10 @@ class PipesTest extends FunSuite {
     implicit object readInt extends Read[Int] {
       override def read(s: String): Int = s.toInt
     }
-
   }
 
   test("pipes") {
     import cont.Cont._
-
-    type Pipe[I, O, A] = A :#: (InCont[I] => OutCont[O] => Unit)
-    trait InCont[I] extends (OutCont[I] => Unit)
-    trait OutCont[O] extends (O => InCont[O] => Unit)
-
-    def InCont[I](k: OutCont[I] => Unit): InCont[I] = k(_)
-
-    def OutCont[O](k: O => InCont[O] => Unit): OutCont[O] = k(_)
-
-    def input[I, O]: Pipe[I, O, I] = k => ki => ko =>
-      ki(OutCont(i => ki1 => k(i)(ki1)(ko)))
-
-    def output[I, O](o: O): Pipe[I, O, Unit] = k => ki => ko =>
-      ko(o)(InCont(ko1 => k()(ki)(ko1)))
-
-    def merge[I, O, M, A](p: Pipe[I, M, A], q: Pipe[M, O, A]): Pipe[I, O, A] =
-      k => ki => ko => q(_ => ???)(InCont(ko1 => p(_ => ???)(ki)(ko1)))(ko)
-
-    def runPipeIO[I: Read, O](p: Pipe[I, O, Unit])(r: BufferedReader) = {
-      lazy val ki: InCont[I] = InCont(_ (Read[I].readLine(r))(ki))
-      lazy val ko: OutCont[O] = OutCont { o => k => println(o); k(ko) }
-      p(_ => _ => _ => ())(ki)(ko)
-    }
-
-    //////
-
-    def double(): Pipe[Int, Int, Unit] = for {
-      i <- input[Int, Int]
-      _ <- output[Int, Int](i * 2)
-      _ <- double()
-    } yield ()
-
-    def quad: Pipe[Int, Int, Unit] = merge(double(), double())
-
-    println(Try(runPipeIO(quad)(
-      new BufferedReader(new StringReader(
-        "1\n" * 100
-      )))))
-  }
-
-  test("pipes2") {
-    type Cont[A, S, R] = (A => S) => R
-    type :#:[A, R] = Cont[A, R, R]
-
-    @inline def pure[A, R](a: A): A :#: R = _ (a)
-
-    @inline def bind[A, S1, R, B, S2](c: (A => S1) => R)(f: A => (B => S2) => S1): (B => S2) => R =
-      k => c(f(_)(k))
-
-    @inline def fmap[A, S, R, B](c: (A => S) => R)(f: A => B): (B => S) => R =
-      k => c(a => k(f(a)))
-
-    implicit class ContMonad[A, S, R](val c: (A => S) => R) {
-      @inline def flatMap[B, C](f: A => Cont[B, C, S]): Cont[B, C, R] = bind(c)(f)
-
-      @inline def map[B](f: A => B): Cont[B, S, R] = fmap(c)(f)
-    }
 
     type Pipe[I, O, A] = A :#: (InCont[I] => OutCont[O] => Unit)
     trait InCont[I] extends (OutCont[I] => Unit)
